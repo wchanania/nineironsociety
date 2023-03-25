aa54f7ed0d663e4e2f183e31294ebae36e9672dc
wchanania <willchanania@gmail.com>
2023-03-24 22:22:57 -0400

    worked on login authentication api

--- backend/base.py
+++ backend/base.py
@@ -1,6 +1,49 @@
-from flask import Flask
+import json
+from flask import Flask, request, jsonify
+from datetime import datetime, timedelta, timezone
+from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
+                               unset_jwt_cookies, jwt_required, JWTManager
+
 
 api = Flask(__name__)
+
+api.config["JWT_SECRET_KEY"] = "WESTHAMLIVERPOOL"
+api.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+jwt = JWTManager(api)
+
+@api.route('/token', methods=['GET','POST'])
+def create_token():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return {"msg": "Wrong usernamew or password"}, 401
+
+    access_token = create_access_token(identity=username)
+    response = {"access_token":access_token}
+    return response
+
+@api.route("/logout", methods=["POST"])
+def logout():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
+
+@api.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            data = response.get_json()
+            if type(data) is dict:
+                data["access_token"] = access_token 
+                response.data = json.dumps(data)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+        return response
 
 @api.route('/profile')
 def my_profile():
